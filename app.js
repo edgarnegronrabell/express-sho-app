fe5b3593a097a78b4a4ee9e070e3c96db432c2d5
Edgar Negron <edgar.negron.rabell@gmail.com>
2020-10-12 08:35:33 -0400

    Add prettier config and create 404 template

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 //Middleware
-app.use(bodyParser.urlencoded( {extended: false} ))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use('/', shopRoutes)
 
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
-
 
 const PORT = process.env.PORT || 3000
 
